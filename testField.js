ca73bff2eb628286612f7f769264bff551920d27
HwangRangyu <tourbillion42@gmail.com>
2024-04-10 15:32:09 +0900

    add tesField

---
+++ testField.js
@@ -0,0 +1,17 @@
+var ss = [12, 9, 3, 24, 2, 19, 10, 6, 5]
+var st1 = [1,2,3,2], st2 = [9,1,4,4]
+var suZero = [-4,-3,-1,0,1,4,5,10]
+var conUniVal = [1,1,1,2,2,3,4,5,5,5,6,6,7]
+var maxSum = [2,6,9,2,1,8,5,6,3]
+var search = [1,2,3,4,5,7,8,9,12,13,15,19,20]
+var radix = [345,5467,12,2345,23,49852]
+var zig = "PAYPALISHIRING"
+var two = [11, 2, 14, 7]
+ 
+// linear search 선형 검색 , 순차 검색
+// binary search 이진 검색 , 이분 검색 - 반으로 나누어서 연산 
+// output: PAHNAPLSIIGYIR
+// binary search 이진 검색 , 이분 검색 - 반으로 나누어서 연산 
+// output: PAHNAPLSIIGYIR
+
+
